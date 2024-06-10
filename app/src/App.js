bd6b6e0299108ca14fb5f61049e843cff1e1c14d
JULIAN FURLAND OTERO <47392040@ort>
2024-06-10 10:46:06 -0300

    10/6

--- app/src/App.js
+++ app/src/App.js
@@ -5,22 +5,7 @@ import { useState } from 'react';
 
 function App() {
   
-const [arrayTurnos, setArrayturnos] = useState([
-  {
-    "mascotaName": "m1",
-    "duenio": "d1",
-    "fechaTurno": "f1",
-    "horaTurno": "h1",
-    "sintomas": "s1"
-  },
-  {
-    "mascotaName": "m2",
-    "duenio": "d2",
-    "fechaTurno": "f2",
-    "horaTurno": "h2",
-    "sintomas": "s2"
-  }
-]);
+const [arrayTurnos, setArrayturnos] = useState([]);
 const addCita = (cita) => {
   setArrayturnos([...arrayTurnos, cita]);
   console.log(cita)
